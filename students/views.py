b6e06f1f0882db8e47d05a679b8f0b7d727b08f1
Blackroad <VladVell@gmail.com>
2019-01-15 23:56:01 +0200

    view created

--- students/views.py
+++ students/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import Http404
+from django.http import HttpResponse
 # Create your views here.
+
+def students_list(request, sid):
+    try:
+        sid = int(sid)
+    except ValueError:
+        raise Http404
+    return render(request, 'demo.html', {})
