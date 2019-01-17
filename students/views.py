6a39ccd03e6a3926c4c64c4557879c1164b61400
vladvell <vladvell@gmail.com>
2019-01-17 19:10:01 +0200

    Added new Students application

--- students/views.py
+++ students/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from django.http import Http404
 from django.http import HttpResponse
 # Create your views here.
 
-def students_list(request, sid):
-    try:
-        sid = int(sid)
-    except ValueError:
-        raise Http404
-    return render(request, 'demo.html', {})
+
+def students_list(request):
+    return render(request, 'students_list.html', {})
