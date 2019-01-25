9318936a57f97abdc34009de37208a00944563e0
vladvell <vladvell@gmail.com>
2019-01-25 17:30:40 +0200

    Students list с наследованием готов

--- students/views.py
+++ students/views.py
@@ -5,7 +5,25 @@ from django.http import HttpResponse
 
 # Views for Students
 def students_list(request):
-    return render(request, 'students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name':u'Віталій',
+         'last_name':u'Подоба',
+         'ticket': 235,
+         'image':'img/podoba3.jpg'},
+        {
+          'id': 2,
+         'first_name': u'Корост',
+         'last_name': u'Андрій',
+         'ticket': 2123 ,
+         'image': 'img/me.jpeg'},
+        {'id': 3,
+         'first_name': u'Притула',
+         'last_name': u'Тарас',
+         'ticket': 3312,
+         'image': 'img/piv.png'},
+    )
+    return render(request, 'students_list.html', {'students':students})
 
 
 def students_add(request):
