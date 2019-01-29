1365e75966717759d229e9c33ec71cdb08c0bd2d
Blackroad <vladvell@gmail.com>
2019-01-29 12:58:46 +0200

    Tab selector is fixed

--- Django/settings.py
+++ Django/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -63,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+
             ],
         },
     },
