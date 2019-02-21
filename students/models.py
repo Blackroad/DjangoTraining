57c790d2ca578776b4314e4ee7aadf9ca7cafba5
vladvell <vladvell@gmail.com>
2019-02-21 18:15:14 +0200

    Added sorting for 1st column

--- students/models.py
+++ students/models.py
@@ -44,5 +44,35 @@ class Student(models.Model):
         blank=True,
         verbose_name=u"Додаткові нотатки")
 
+    student_group = models.ForeignKey('Group', verbose_name=u"Група", blank=False,  null=True, on_delete=models.PROTECT)
+
     def __str__(self):
         return u"%s %s" % (self.first_name, self.last_name)
+
+class Group(models.Model):
+    """Student Model"""
+    class Meta(object):
+        verbose_name = u"Група"
+        verbose_name_plural = u"Групи"
+
+    title = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Назва")
+
+    leader = models.OneToOneField('Student',
+        max_length=246,
+        blank=True,
+        null= True,
+        on_delete=models.SET_NULL,
+        verbose_name=u"Староста")
+
+    notes = models.TextField(
+        blank=True,
+        verbose_name=u"Додаткові нотатки")
+
+    def __str__(self):
+        if self.leader:
+            return u"%s %s %s" % (self.title, self.leader.first_name, self.leader.last_name)
+        else:
+            return u"%s" % (self.title,)
