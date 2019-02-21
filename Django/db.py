1a56c1c97e68526f7645e5e139b4e7f3e271f497
vladvell <vladvell@gmail.com>
2019-02-21 18:15:40 +0200

    Added sorting for 1st column

---
+++ Django/db.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'students_db_user',
+        'PASSWORD': '1234',
+        'NAME': 'students_db',
+    }
+}
