ae3a0ec0eec81c24808a76b58f7b9de23f9d3c80
vladvell <vladvell@gmail.com>
2019-03-01 12:14:29 +0200

    Added logic for rendering days and weekdays and current month for visits with comments Added new class 'DateTranslation' to store month and weekdays Visit template is modified appropriate to new implemented logic in visits view

---
+++ students/static/localization_data/date_translation.py
@@ -0,0 +1,4 @@
+class DateTranslation:
+    days_urk = {'Mon': 'Пн', 'Tue': 'Вт', 'Wed': 'Cр', 'Thu': 'Чт', 'Fri': 'Пт', 'Sat': 'Сб', 'Sun': 'Вс'}
+    month_local = {'January': 'Січень', 'February': 'Лютий', 'March': 'Березень', "April": "Квітень", "May": "Травень", "June": "Червень", "July": "Липень",
+                   "August": "Серпень", "September": "Вересень", "October": "Жовтень", "November": "Листопад", "December": "Грудень"}
--- students/views/visits.py
+++ students/views/visits.py
@@ -1,22 +1,34 @@
 from django.shortcuts import render
 from ..model.students import Student
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from ..static.localization_data.date_translation import DateTranslation
 import datetime
 
 
+def get_days():
+    # get date from local machine
+    today = datetime.date.today()
+    # calculate number of days in current month and create a list for it
+    days_number = datetime.date(today.year, today.month + 1, 1) - datetime.date(today.year, today.month, 1)
+    days = [i for i in range(1, int(days_number.days) + 1)]
+    # create list of named weekdays for each day using ukr translation from imported DateTranslation class
+    ukr_day = [DateTranslation.days_urk.get(i) for i in [i for i in [datetime.date(today.year, today.month, i).strftime('%a') for i in range(1, int(days_number.days) + 1)]]]
+    # compiling number of days in month with weekdays names to receive list like [[Пн,1],[Вт,2]...]
+    compile_days = list(map(list, zip(ukr_day, days)))
+    # packing all in list with format ['Пн,1' , 'Вт,2,', ....] and return it
+    list_default = []
+    for i in compile_days:
+        list_default.append(('{} {}'.format(i[0], i[1])))
+    compile_days = list_default
+    return compile_days
+
+
 def visits_list(request):
-    monthlocal = {'February': 'Лютий', 'Березень': 'March'}
-
-    days_urk = {'Mon': 'Пн', 'Tue': 'Вт', 'Wed': 'Cр', 'Thu': 'Чт', 'Fri': 'Пт', 'Sat': 'Сб', 'Sun': 'Вс'}
+    # Get translated  month from localization class
+    all_month = DateTranslation.month_local
+    # Get current month in from system date
     current_month = (datetime.date.today()).strftime('%B')
-    def get_days():
-        today = datetime.date.today()
-        days_number = datetime.date(today.year, today.month + 1, 1) - datetime.date(today.year, today.month, 1)
-        days = [i for i in range(1, int(days_number.days) + 1)]
-        urk_day = [days_urk.get(i) for i in [i for i in [datetime.date(today.year, today.month, i).strftime('%a') for i in range(1, int(days_number.days))]]]
-        return(days, urk_day)
-
-    students = Student.objects.values('first_name','last_name')
-    visits = students
-    return render(request, 'visits.html', {'visits': visits, 'days': get_days, 'current_month': monthlocal.get(current_month), 'ukr_days':get_days})
-
+    # get compile weekdays with number days
+    compile_days = get_days()
+    visits = Student.objects.values('first_name', 'last_name')
+    return render(request, 'visits.html', {'visits': visits, 'days': compile_days, 'current_month': all_month.get(current_month)})
